0bf18c452a8f2ee1b510f97e662275879bab7ff8
Wen Kiong <deadfiesta@gmail.com>
2025-03-07 14:05:43 +0800

    Uininstalled a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,8 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-designs",
-    "@storybook/addon-a11y"
+    "@storybook/addon-designs"
   ],
   framework: {
     name: "@storybook/react-vite",
